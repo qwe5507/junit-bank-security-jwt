aac261359faaa202ed828c424c7971b574cc55d0
qwe5507 <apvmffkdls@gmail.com>
2023-03-23 12:23:59 +0900

    validation 정규식 마무리

--- src/main/java/shop/mtcoding/bank/dto/user/UserResDto.java
+++ src/main/java/shop/mtcoding/bank/dto/user/UserResDto.java
@@ -8,6 +8,7 @@ import shop.mtcoding.bank.domain.user.UserEnum;
 
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 
 public class UserResDto {
     @Setter
@@ -17,13 +18,19 @@ public class UserResDto {
         @Pattern(regexp = "^[a-zA-Z0-9]{2,20}$", message = "영문/숫자 2~20자 이내로 작성해주세요")
         @NotEmpty // null이거나, 공백일 수 없다.
         private String username;
+
         // 길이 4~20
+        @Size(min = 4, max = 20) // Size는 String만 가능
         @NotEmpty
         private String password;
+
         // 이메일 형식
+        @Pattern(regexp = "^[a-zA-Z0-9]{2,10}@[a-zA-Z0-9]{2,6}\\.[a-zA-Z]{2,3}$", message = "이메일 형식으로 작성해주세요.")
         @NotEmpty
         private String email;
+
         // 영어, 한글, 1 ~20
+        @Pattern(regexp = "^[a-zA-Z가-힣]{1,20}$", message = "한글/영문 1~20자 이내로 작성해주세요.")
         @NotEmpty
         private String fullname;
 
--- src/test/java/shop/mtcoding/bank/temp/RegexTest.java
+++ src/test/java/shop/mtcoding/bank/temp/RegexTest.java
@@ -49,4 +49,32 @@ public class RegexTest {
         boolean result = Pattern.matches("^[a-zA-Z]{2,4}$", value); // 최소 1번 이상인 + 대신, + 자리에 {}안에 최소, 최대 길이 지정
         Assertions.assertTrue(result);
     }
+
+    // username, email, fullname
+    @Test
+    public void user_username_test() throws Exception {
+        String username ="ssar";
+        // 영문 숫자 2~20자 이내
+        boolean result = Pattern.matches("^[a-zA-Z0-9]{2,20}$", username);
+        Assertions.assertTrue(result);
+    }
+
+    @Test
+    public void user_fullname_test() throws Exception {
+        String username ="ssa가r";
+        // 영문, 한글 1~20
+        boolean result = Pattern.matches("^[a-zA-Z가-힣]{1,20}$", username);
+        Assertions.assertTrue(result);
+    }
+
+    @Test
+    public void user_email_test() throws Exception {
+        String email ="ssar@nate.com";
+        // 연습겸 직접 썻지만, 이메일은 validation은 라이브러리 사용하는거 권장
+        boolean result = Pattern.matches("^[a-zA-Z0-9]{2,10}@[a-zA-Z0-9]{2,6}\\.[a-zA-Z]{2,3}$", email);
+        // 영어, 숫자가 2글자에서 10글자 후 골뱅이(@), @ 후에 영어, 숫자 2글자 에서 6글자 후 점(.)후에 영어 2,3글자
+        // com만 있다고 가정, 완벽한 이메일정규표현식은 아님
+        Assertions.assertTrue(result);
+    }
+
 }
