baa94a4fb4e5cfd3832e53ccc57d7b646ccb33be
qwe5507 <apvmffkdls@gmail.com>
2023-03-25 13:34:51 +0900

    Jwt Filter등록

--- src/main/java/shop/mtcoding/bank/config/SecurityConfig.java
+++ src/main/java/shop/mtcoding/bank/config/SecurityConfig.java
@@ -1,19 +1,20 @@
 package shop.mtcoding.bank.config;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import shop.mtcoding.bank.config.jwt.JwtAuthenticationFilter;
 import shop.mtcoding.bank.domain.user.UserEnum;
-import shop.mtcoding.bank.dto.ResponseDto;
 import shop.mtcoding.bank.util.CustomResponseUtil;
 
 @Configuration
@@ -27,10 +28,18 @@ public class SecurityConfig {
     }
 
     // JWT 필터 등록이 필요함
+    public class CustomSecurityFilterManager extends AbstractHttpConfigurer<CustomSecurityFilterManager, HttpSecurity> {
+        @Override
+        public void configure(HttpSecurity builder) throws Exception {
+            AuthenticationManager authenticationManager = builder.getSharedObject(AuthenticationManager.class);
+            builder.addFilter(new JwtAuthenticationFilter(authenticationManager));
+            super.configure(builder);
+        }
+    }
 
     // JWT 서버를 만들 예정!! Session 사용안함.
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         log.debug("디버그 : filterChain 빈 등록됨");
         http.headers().frameOptions().disable(); // iframe 허용 안함
         http.csrf().disable(); // enable이면 요청 시에 CSRF 토큰 값을 보내야 함
@@ -43,6 +52,9 @@ public class SecurityConfig {
         // httpBasic은 브라우저가 팝업창을 이용해서 사용자 인증을 진행한다.
         http.httpBasic().disable(); // formLogin과 겹치면 formLogin이 우선시 된다 ,
 
+        // 필터 적용
+        http.apply(new CustomSecurityFilterManager());
+
         // 인증 실패 시 Exception 가로채기
         // authenticationEntryPoint를 커스텀 하면 API요청, 브라우저 요청 둘 다 인증 에러 결과가 동일 해짐
         http.exceptionHandling().authenticationEntryPoint((request, response, authException) -> {
@@ -51,7 +63,7 @@ public class SecurityConfig {
 
         http.authorizeRequests()
                 .antMatchers("/api/s/**").authenticated()
-                .antMatchers("/api/admin/**").hasRole(""+ UserEnum.ADMIN) // 최근 공식문서에서는 ROLE_ 안붙여도 됨
+                .antMatchers("/api/admin/**").hasRole("" + UserEnum.ADMIN) // 최근 공식문서에서는 ROLE_ 안붙여도 됨
                 .anyRequest().permitAll();
 
         return http.build();
@@ -69,4 +81,5 @@ public class SecurityConfig {
         source.registerCorsConfiguration("/**", configuration); // 주소로 요청이 와도 위의 cors 설정 적용
         return source;
     }
+
 }
--- src/main/java/shop/mtcoding/bank/config/jwt/JwtAuthenticationFilter.java
+++ src/main/java/shop/mtcoding/bank/config/jwt/JwtAuthenticationFilter.java
@@ -1,6 +1,8 @@
 package shop.mtcoding.bank.config.jwt;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.InternalAuthenticationServiceException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -19,7 +21,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
-
+    private final Logger log = LoggerFactory.getLogger(getClass());
     private final AuthenticationManager authenticationManager;
 
     public JwtAuthenticationFilter(AuthenticationManager authenticationManager) {
@@ -28,10 +30,10 @@ public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilte
         this.authenticationManager = authenticationManager;
     }
 
-    // Post : "/login"에 동작 함
-    // /api/login으로 변경
+    // Post : "/login"에 동작 함 -> /api/login으로 변경
     @Override
     public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
+        log.debug("디버그 : attemptAuthentication 호출됨");
         try {
             ObjectMapper om = new ObjectMapper();
             // 컨트롤러 가기 전이라서, 이 객체의 username, password에는 Spring Validation 적용 불가
@@ -53,6 +55,7 @@ public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilte
     // 위의 return authentication이 잘 작동하면 해당 메소드가 호출 됩니다.
     @Override
     protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException, ServletException {
+        log.debug("디버그 : successfulAuthentication 호출됨");
         LoginUser loginUser = (LoginUser) authResult.getPrincipal();
         String jwtToken = JwtProcess.create(loginUser);
         response.addHeader(JwtVO.HEADER, jwtToken);
