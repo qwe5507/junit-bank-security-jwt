4ce59b3ee21150114b602765503860304700c2e1
qwe5507 <apvmffkdls@gmail.com>
2023-03-25 19:07:27 +0900

    jwt 인증 필터 테스트

--- src/test/java/shop/mtcoding/bank/config/jwt/JwtAuthenticationFilterTest.java
+++ src/test/java/shop/mtcoding/bank/config/jwt/JwtAuthenticationFilterTest.java
@@ -1,7 +1,80 @@
 package shop.mtcoding.bank.config.jwt;
 
-import static org.junit.jupiter.api.Assertions.*;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.transaction.annotation.Transactional;
+import shop.mtcoding.bank.config.dummy.DummyObject;
+import shop.mtcoding.bank.domain.user.User;
+import shop.mtcoding.bank.domain.user.UserRepository;
+import shop.mtcoding.bank.dto.user.UserReqDto.LoginReqDto;
 
-class JwtAuthenticationFilterTest {
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@Transactional
+@ActiveProfiles("test")
+@AutoConfigureMockMvc
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+class JwtAuthenticationFilterTest extends DummyObject {
+
+    @Autowired
+    private ObjectMapper om;
+    @Autowired
+    private MockMvc mvc;
+    @Autowired
+    private UserRepository userRepository;
+
+    @BeforeEach
+    public void setUp() throws Exception {
+        User ssar = userRepository.save(newUser("ssar", "쌀"));
+    }
+
+    @Test
+    public void successfulAuthentication_test() throws Exception {
+        // given
+        LoginReqDto loginReqDto = new LoginReqDto();
+        loginReqDto.setUsername("ssar");
+        loginReqDto.setPassword("1234");
+        String requestBody = om.writeValueAsString(loginReqDto);
+
+        // when
+        ResultActions resultActions = mvc.perform(post("/api/login").content(requestBody).contentType(MediaType.APPLICATION_JSON));
+        String responseBody = resultActions.andReturn().getResponse().getContentAsString();
+        String jwtToken = resultActions.andReturn().getResponse().getHeader(JwtVO.HEADER);
+
+        // then
+        resultActions.andExpect(status().isOk());
+        assertNotNull(jwtToken);
+        assertTrue(jwtToken.startsWith(JwtVO.TOKEN_PREFIX));
+        resultActions.andExpect(jsonPath("$.data.username").value("ssar"));
+    }
+
+    @Test
+    public void unsuccessfulAuthentication_test() throws Exception {
+        // given
+        LoginReqDto loginReqDto = new LoginReqDto();
+        loginReqDto.setUsername("ssar");
+        loginReqDto.setPassword("12345"); // 비밀번호 틀림
+        String requestBody = om.writeValueAsString(loginReqDto);
+
+        // when
+        ResultActions resultActions = mvc.perform(post("/api/login").content(requestBody).contentType(MediaType.APPLICATION_JSON));
+        String responseBody = resultActions.andReturn().getResponse().getContentAsString();
+        String jwtToken = resultActions.andReturn().getResponse().getHeader(JwtVO.HEADER);
+        System.out.println(responseBody);
+
+        // then
+        resultActions.andExpect(status().isUnauthorized());
+    }
 }
