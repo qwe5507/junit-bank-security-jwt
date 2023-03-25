b108c33f2be021eecbc7afcb88a6d659027e6f3d
qwe5507 <apvmffkdls@gmail.com>
2023-03-25 19:23:26 +0900

    jwt 인가 필터 테스트

--- src/test/java/shop/mtcoding/bank/config/jwt/JwtAuthorizationFilterTest.java
+++ src/test/java/shop/mtcoding/bank/config/jwt/JwtAuthorizationFilterTest.java
@@ -1,7 +1,64 @@
 package shop.mtcoding.bank.config.jwt;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import shop.mtcoding.bank.config.auth.LoginUser;
+import shop.mtcoding.bank.domain.user.User;
+import shop.mtcoding.bank.domain.user.UserEnum;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ActiveProfiles("test")
+@AutoConfigureMockMvc
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
 class JwtAuthorizationFilterTest {
+    @Autowired
+    private MockMvc mvc;
 
+    @Test
+    public void authorization_success_test() throws Exception {
+        // given
+        User user = User.builder().id(1L).role(UserEnum.CUSTOMER).build();
+        LoginUser loginUser = new LoginUser(user);
+        String jwtToken = JwtProcess.create(loginUser);
+        System.out.println("테스트 : " + jwtToken);
+
+        // when
+        ResultActions resultActions = mvc.perform(get("/api/s/hello/test").header(JwtVO.HEADER, jwtToken)); // 인증이 필요한 url
+
+        // then
+        resultActions.andExpect(status().isNotFound()); // 인증이 필요하지만 실제 존재하지 않는 url이기 때문에, 404 뜨는게 맞음 (인증 실패면 401, 권한없으면 403)
+    }
+
+    @Test
+    public void authorization_fail_test() throws Exception {
+        // given
+
+        // when
+        ResultActions resultActions = mvc.perform(get("/api/s/hello/test")); // 인증이 필요한 url
+
+        // then
+        resultActions.andExpect(status().isUnauthorized()); // jwtToken없으면 401
+    }
+
+    @Test
+    public void authorization_admin_test() throws Exception {
+        // given
+        User user = User.builder().id(1L).role(UserEnum.CUSTOMER).build();
+        LoginUser loginUser = new LoginUser(user);
+        String jwtToken = JwtProcess.create(loginUser);
+        System.out.println("테스트 : " + jwtToken);
+
+        // when
+        ResultActions resultActions = mvc.perform(get("/api/admin/hello/test").header(JwtVO.HEADER, jwtToken)); // ADMIN권한이 필요한 url
+
+        // then
+        resultActions.andExpect(status().isForbidden()); // URL은 ADMIN권한이 필요하지만 CUSTOMER권한의 토큰이기 떄문에 403 발생
+    }
 }
