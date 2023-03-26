829e97399635008b5d3d4b2656bbf3b3662dae0c
qwe5507 <apvmffkdls@gmail.com>
2023-03-26 18:16:47 +0900

    계좌등록 서비스 테스트

---
+++ src/test/java/shop/mtcoding/bank/service/AccountServiceTest.java
@@ -0,0 +1,67 @@
+package shop.mtcoding.bank.service;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Spy;
+import org.mockito.junit.jupiter.MockitoExtension;
+import shop.mtcoding.bank.config.dummy.DummyObject;
+import shop.mtcoding.bank.domain.account.Account;
+import shop.mtcoding.bank.domain.account.AccountRepository;
+import shop.mtcoding.bank.domain.user.User;
+import shop.mtcoding.bank.domain.user.UserRepository;
+import shop.mtcoding.bank.dto.account.AccountReqDto.AccountSaveReqDto;
+import shop.mtcoding.bank.dto.account.AccountResDto.AccountSaveResDto;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class AccountServiceTest extends DummyObject {
+    @InjectMocks
+    private AccountService accountService;
+
+    @Mock
+    private UserRepository userRepository;
+
+    @Mock
+    private AccountRepository accountRepository;
+
+    @Spy
+    private ObjectMapper om;
+
+    @Test
+    public void 계좌등록_test() throws Exception {
+        // given
+        Long userId = 1L;
+
+        AccountSaveReqDto accountSaveReqDto = new AccountSaveReqDto();
+        accountSaveReqDto.setNumber(1111L);
+        accountSaveReqDto.setPassword(1234L);
+
+        // stub1
+        User ssar = newMockUser(userId, "ssar", "쌀");
+        when(userRepository.findById(any())).thenReturn(Optional.of(ssar));
+
+        // stub2
+        when(accountRepository.findByNumber(any())).thenReturn(Optional.empty());
+
+        // stub3
+        Account ssarAccount = newMockAccount(1L, 1111L, 1000L, ssar);
+        when(accountRepository.save(any())).thenReturn(ssarAccount);
+
+        // when
+        AccountSaveResDto accountSaveResDto = accountService.계좌등록(accountSaveReqDto, userId);
+        String s = om.writeValueAsString(accountSaveResDto);
+        System.out.println();
+
+        // then
+        assertThat(accountSaveResDto.getNumber()).isEqualTo(1111L);
+    }
+
+}
